47b25a46b226092e0fea6de7bc3c7c0b68df049a
Jason Kaufmann <jasonkaufmann07@gmail.com>
2022-10-23 14:17:30 -0700

    Initial commit

---
+++ srLatch/srLatch.v
@@ -0,0 +1,14 @@
+`timescale 1 ns / 100 ps
+
+//NOTE: this is not routable on the ice40 FPGA. This is only for simulation purposes.
+
+module srLatch (
+    input set,
+    input reset,
+    output q,
+    output qNot);
+
+    assign #(1) qNot = ~(set | q);
+    assign #(1) q = ~(reset | qNot);
+
+endmodule
